75d52184a3f73a279f48a2bae7cb39e15be85642
Путинцев Дмитрий <putdv@mail.ru>
2025-11-26 14:40:39 +0300

    Четвертое задание

---
+++ Ex4_Melon/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 26 14:25:07 2025
+
+@author: TBG
+"""
+
+print("Введите массу арбуза")
+w = int(input())
+
+if w % 2 == 0 and w >= 4:
+    print("YES")
+else:
+    print("NO")
+    
